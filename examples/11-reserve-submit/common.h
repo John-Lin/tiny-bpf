8e66813cd62836a9fda47e20dc0fa791f85030a1
johnlin <linton.tw@gmail.com>
2025-11-07 01:34:25 +0800

    add examples

---
+++ examples/11-reserve-submit/common.h
@@ -0,0 +1,12 @@
+#ifndef __COMMON_H
+#define __COMMON_H
+
+/* definition of a sample sent to user-space from BPF program */
+struct event {
+  int pid;
+  int uid;
+  char command[16];
+  char filename[512];
+};
+
+#endif /* __COMMON_H */
